abe7727ef511186f6431d4b85cc66ae001298bd0
jaeheon <>
2022-08-04 21:51:26 +0900

    [최재헌] - sns 세팅

---
+++ src/main/java/com/eee0/SNS/SnsApplication.java
@@ -0,0 +1,13 @@
+package com.eee0.SNS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eee0/SNS/SnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eee0.SNS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
